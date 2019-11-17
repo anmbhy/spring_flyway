127bb26ff63d109fc78a1ff55850066a5a93d738
anmbhy <anomyous.me@gmail.com>
2019-11-17 21:47:17 +0700

    Initial commit

---
+++ src/main/java/com/anmbhy/springflyway/SpringflywayApplication.java
@@ -0,0 +1,14 @@
+package com.anmbhy.springflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringflywayApplication {
+
+	// test
+	public static void main(String[] args) {
+		SpringApplication.run(SpringflywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anmbhy/springflyway/SpringflywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmbhy.springflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringflywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
